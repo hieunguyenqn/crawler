e3deef3e55adece4c0b7cd7eab39d088cd842857
Mac Browning <mac@macasaurus.com>
2014-02-03 01:01:09 -0800

    Safe map Add

--- crawler/safe_map_test.go
+++ crawler/safe_map_test.go
@@ -10,3 +10,17 @@ func Test_safeMap_NewsafeMap(t *testing.T) {
     t.Errorf("Map should be initialized.")
   }
 }
+
+func Test_safeMap_Add(t *testing.T) {
+  type Temp struct {
+    Name string
+  }
+  temp := &Temp{Name: "test"}
+  s := newSafeMap()
+
+  s.Add(temp.Name, temp)
+  ret := s.data[temp.Name]
+  if ret != temp {
+    t.Errorf("%v did not match %v", ret, temp)
+  }
+}
