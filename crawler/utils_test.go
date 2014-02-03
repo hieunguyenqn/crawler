1f465c5513192d63e55c3bab5f7bcc78f5ac9912
Mac Browning <mac@macasaurus.com>
2014-02-03 01:00:55 -0800

    Pages and assets utils

--- crawler/utils_test.go
+++ crawler/utils_test.go
@@ -1,10 +1,45 @@
 package crawler
 
 import (
-  "net/url"
+  "fmt"
+  "math/rand"
+  "time"
 )
 
+func init() {
+  rand.Seed(time.Now().UnixNano())
+}
+
 func newTestPage() *Page {
-  u, _ := url.Parse("http://www.example.com")
-  return NewPage(u)
+  p, e := NewPageFromString(fmt.Sprintf("http://www.example.com/%d", rand.Uint32()))
+  if e != nil {
+    panic(e)
+  }
+  return p
+}
+
+func newTestAsset() *Asset {
+  a, e := NewAssetFromString(fmt.Sprintf("http://www.example.com/%d", rand.Uint32()))
+  if e != nil {
+    panic(e)
+  }
+  return a
+
+}
+
+func newTestNestedPages() *Page {
+  p := newTestPage()
+  for i := 0; i < 3; i++ {
+    subpage := newTestPage()
+    asset := newTestAsset()
+    p.Links = append(p.Links, subpage)
+    p.Assets = append(p.Assets, asset)
+    for x := 0; x < 3; x++ {
+      secondSubpage := newTestPage()
+      secondAsset := newTestAsset()
+      subpage.Links = append(subpage.Links, secondSubpage)
+      subpage.Assets = append(subpage.Assets, secondAsset)
+    }
+  }
+  return p
 }
