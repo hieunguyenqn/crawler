c496bb44a3695104ca436e9855f010ff52dd2281
Mac Browning <mac@macasaurus.com>
2014-02-02 22:11:54 -0800

    Write output to json. Breaks on circular relationship.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "encoding/json"
   "fmt"
   "github.com/macb/crawler/crawler"
   "os"
@@ -17,28 +18,38 @@ func main() {
     defer pprof.StopCPUProfile()
 
   }
-  crawl("http://www.macasaurus.com", false)
+  crawl("http://www.macasaurus.com", true)
   crawl("http://www.devbootcamp.com", false)
   crawl("http://www.digitalocean.com", false)
 }
 
-func printFirstLevel(page *crawler.Page) {
-  fmt.Println("Printing First Level: ", page.URL)
-  for _, p := range page.Links {
-    fmt.Println("Subpage: ", p.URL)
-    for _, a := range p.Assets {
-      fmt.Println("    ", a.URL)
-    }
+func save(page *crawler.Page) {
+  filename := "/tmp/" + page.URL.Host + ".json"
+  by, e := json.Marshal(page)
+  if e != nil {
+    panic(e)
   }
+  f, e := os.Create(filename)
+  if e != nil {
+    panic(e)
+  }
+  defer f.Close()
+
+  _, e = f.Write(by)
+  if e != nil {
+    panic(e)
+  }
+  fmt.Println("Your results are at: " + filename)
 }
 
-func crawl(url string, printResults bool) {
+func crawl(url string, saveResults bool) {
   start := time.Now()
   page, job := crawler.Crawl(url)
-  if printResults {
-    printFirstLevel(page)
-  }
   stop := time.Now()
+
+  if saveResults {
+    save(page)
+  }
   duration := stop.Sub(start)
   fmt.Printf("Starting from %s, crawled %d pages in %s\n", page.URL, job.PagesCrawled, duration)
 }
